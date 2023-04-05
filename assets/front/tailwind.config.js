91b2d97e8c60db7e912e713e3ce74c7adab2e75b
Amélia Massot <ameliamassot@gmail.com>
2023-04-05 13:33:17 +0200

    Adding the design of the site

--- assets/front/tailwind.config.js
+++ assets/front/tailwind.config.js
@@ -13,6 +13,24 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        green: '#015f5a',
+        orange: '#FFAF38',
+        'dark-orange': '#f0a638',
+        blue: '#abd1c6',
+        white: '#fffffe',
+        black: '#001e1d',
+        brown: '#996A23',
+      },
+      backgroundImage: {
+        'circle': "url('./images/circle.svg')",
+      },
+      backgroundSize: {
+        '80': '80%',
+      },
+      screens: {
+        '4xl': '2000px',
+      },
     },
   },
   variants: {},
